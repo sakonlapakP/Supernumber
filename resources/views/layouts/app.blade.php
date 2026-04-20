026ba7f5aa8bcf6cbd2d98d7543a07a2f3692281
EfAum <efaum@EfAums-iMac.local>
2026-04-20 22:40:27 +0700

    design: update logo typography to match serif/sans hybrid style from reference

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,9 @@
     @hasSection('preload_image')
       <link rel="preload" as="image" href="@yield('preload_image')" />
     @endif
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ $staticPath('css/supernumber.css') }}?v={{ $cssVersion }}" />
   </head>
   <body class="@yield('body_class')">
