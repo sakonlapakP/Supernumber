0dfc24ce7d9888609eab02a76e0612450b1867dd
EfAum <efaum@EfAums-iMac.local>
2026-04-26 12:47:53 +0700

    Fix: Restore standard public folder structure for local compatibility and implement root .htaccess proxy for shared hosting

--- index.php
+++ index.php
@@ -1,21 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
 
-define('LARAVEL_START', microtime(true));
-
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// Register the Composer autoloader...
-require __DIR__.'/vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-$app = require_once __DIR__.'/bootstrap/app.php';
-
-// Tell Laravel that the public path is the current directory (public_html)
-$app->usePublicPath(__DIR__);
-
-$app->handleRequest(Request::capture());
+require_once __DIR__ . '/public/index.php';
