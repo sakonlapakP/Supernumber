62c3a3ed3d6753f3057442339ece87674bb05330
EfAum <efaum@EfAums-iMac.local>
2026-05-09 16:15:10 +0700

    fix: move cache cleaner to public and update paths

--- public/clear_cache_super.php
+++ public/clear_cache_super.php
@@ -4,8 +4,8 @@
 
 define('LARAVEL_START', microtime(true));
 
-require __DIR__ . '/vendor/autoload.php';
-$app = require_once __DIR__ . '/bootstrap/app.php';
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 
